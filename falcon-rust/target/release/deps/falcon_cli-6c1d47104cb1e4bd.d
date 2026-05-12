4a6a77fd38490d269b5feecfc403c6b078aae57f
mmarincic1 <mmarincic1@etf.unsa.ba>
2026-05-12 16:18:42 +0200

    new release package

--- falcon-rust/target/release/deps/falcon_cli-6c1d47104cb1e4bd.d
+++ falcon-rust/target/release/deps/falcon_cli-6c1d47104cb1e4bd.d
@@ -1,5 +1,5 @@
-/Users/matejmarincic/Desktop/Fax/Magistarski/falcon-rust/target/release/deps/falcon_cli-6c1d47104cb1e4bd.d: falcon-cli/src/main.rs
+/Users/matejmarincic/Desktop/Fax/IV Semestar/Magistarski/Selective-disclosure-BLS-Merkle-Trees/falcon-rust/target/release/deps/falcon_cli-6c1d47104cb1e4bd.d: falcon-cli/src/main.rs
 
-/Users/matejmarincic/Desktop/Fax/Magistarski/falcon-rust/target/release/deps/falcon_cli-6c1d47104cb1e4bd: falcon-cli/src/main.rs
+/Users/matejmarincic/Desktop/Fax/IV Semestar/Magistarski/Selective-disclosure-BLS-Merkle-Trees/falcon-rust/target/release/deps/falcon_cli-6c1d47104cb1e4bd: falcon-cli/src/main.rs
 
 falcon-cli/src/main.rs:
